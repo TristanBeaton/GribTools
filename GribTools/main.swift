4b2f115d90818cbab8973113c18b824ccf52b041
Tristan Beaton <tristanbeaton@outlook.co.nz>
2019-06-17 15:13:19 +1200

    Initial Commit

---
+++ GribTools/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GribTools
+//
+//  Created by Tristan Beaton on 17/06/19.
+//  Copyright © 2019 Tristan Beaton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
